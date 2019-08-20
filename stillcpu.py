d0a7203966b849a8b80afab1ea8b73a08d41e8ce
eloy moreno garcia <eloy.moreno@MBE.local>
2019-08-20 09:51:18 +0200

    black and s3

--- stillcpu.py
+++ stillcpu.py
@@ -1,12 +1,19 @@
 import time
+import boto3
+import sys
+
+s3 = boto3.client("s3")
+
 
 def stillCPU():
     a = True
     t = time.time()
-    while(a):
+    while a:
         time.sleep(0.00001)
-        if (time.time() - t)>10:
-            a=False
+        if (time.time() - t) > 10:
+            a = False
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     stillCPU()
+    s3.put_object(Bucket="aws-exercice-bucket", Key=sys.argv[1])
